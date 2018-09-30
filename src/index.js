ef72fe50261945b6a60fca272d46179099419f55
Катрин Райлян <k.shalamowa@gmail.com>
2018-09-30 20:45:42 +0300

    Optimized code

--- src/index.js
+++ src/index.js
@@ -1,44 +1,26 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
- if (currency === 0 || currency < 0){
+ if (currency <= 0){
  			return [];
  } else if (currency >= 10000) {
-     var rich = {error:"You are rich, my friend! We don't have so much coins for exchange"};
+      let rich = {error:"You are rich, my friend! We don't have so much coins for exchange"};
    		return rich; 
  } else {
-    	var coins = [{H:50},{Q:25},{D:10},{N:5},{P:1}]
+     	let h = (Math.floor(currency/50));
+      let ans1 = (currency - (h *50));
+      
+      let q = (Math.floor( ans1 / 25))
+      let ans2 = ans1 - (q * 25);
+      
+      let d = (Math.floor( ans2 / 10))
+      let ans3 = ans2 - (d * 10);
+ 			
+      let n = (Math.floor( ans3 / 5))
+      let ans4 = ans3 - (n * 5);
+ 		
+      let p = (Math.floor(ans4 / 1))	
      
-     	var h = (Math.floor(currency / coins[0].H));
-      var ans1 = currency - (h * coins[0].H);
-      coins[0].H = h; // new value Half-dollars
-      
-      var q = (Math.floor( ans1 / coins[1].Q))
-      var ans2 = ans1 - (q * coins[1].Q);
- 			coins[1].Q = q; // new value Quarters
-      
-      var d = (Math.floor( ans2 / coins[2].D))
-      var ans3 = ans2 - (d * coins[2].D);
- 			coins[2].D = d; // new value Dimes
-      
-      var n = (Math.floor( ans3 / coins[3].N))
-      var ans4 = ans3 - (n * coins[3].N);
- 			coins[3].N = n; // new value Nickels
-      
-      var p = (Math.floor( ans4 / coins[4].P))
-      var ans5 = ans4 - (n * coins[4].P);
- 			coins[4].P = p; // new value Pennies
-    
-      
-//TESTING
-    
-var exchange = {H:h,Q:q,D:d,N:n,P:p};
-
-if(h == 0){delete exchange.H;}
-if(q == 0){delete exchange.Q;}
-if(d == 0){delete exchange.D;}
-if(n == 0){delete exchange.N;}
-if(p == 0){delete exchange.P;}
-
-return exchange;   } // Returns coins needed to make change
+      let completeExchange = {H:h,Q:q,D:d,N:n,P:p};
+      if(completeExchange.value == 0) delete completeExchange.value;    
+      return completeExchange;   
+  } 
 }
-
